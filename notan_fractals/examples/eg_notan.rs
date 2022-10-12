561b9d6a454c1390173392e7ad911b497e543af0
Irfan Baig <riverfr0zen@elitemail.org>
2022-10-12 15:35:31 -0400

    added logging to eg_notan, messing w/ aspect fit

--- notan_fractals/examples/eg_notan.rs
+++ notan_fractals/examples/eg_notan.rs
@@ -1,9 +1,24 @@
 use notan::draw::*;
+use notan::log;
 use notan::prelude::*;
+
 
 #[notan_main]
 fn main() -> Result<(), String> {
-    notan::init().draw(draw).add_config(DrawConfig).build()
+    notan::init()
+        .initialize(starting_logs)
+        .draw(draw)
+        .add_config(log::LogConfig::debug())
+        .add_config(DrawConfig)
+        .build()
+}
+
+
+fn starting_logs() {
+    log::debug!("Hello, this is a debug log...");
+    log::info!("And this is a info log!");
+    log::warn!("I'm warning you");
+    log::error!("I'm an error, I told you...");
 }
 
 fn draw(gfx: &mut Graphics) {
