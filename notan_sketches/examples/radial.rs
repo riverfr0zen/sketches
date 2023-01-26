0c953c14055b227f197e90d638f88b18af32d9d1
Irfan Baig <riverfr0zen@elitemail.org>
2023-01-26 00:38:19 -0500

    use rendered textures for shapes

--- notan_sketches/examples/radial.rs
+++ notan_sketches/examples/radial.rs
@@ -13,7 +13,7 @@ pub struct Node {
     pub pos: Vec2,
     pub last_angle: f32,
     pub active: bool,
-    pub is_central: bool,
+    pub is_parent: bool,
 }
 
 
@@ -21,6 +21,8 @@ pub struct Node {
 pub struct State {
     pub rng: Random,
     pub last_update: f32,
+    pub parent_texture: Texture,
+    pub spawn_texture: Texture,
     pub nodes: Vec<Node>,
     pub parent_radius: f32,
     pub spawn_radius: f32,
@@ -34,15 +36,37 @@ impl State {
 }
 
 
-fn init() -> State {
+fn create_circle_texture(gfx: &mut Graphics, radius: f32, color: Color) -> Texture {
+    let rt = gfx
+        .create_render_texture((radius * 2.0) as i32, (radius * 2.0) as i32)
+        .build()
+        .unwrap();
+    let mut draw = gfx.create_draw();
+    draw.set_size(radius * 2.0, radius * 2.0);
+    draw.circle(radius)
+        .position(radius, radius)
+        .fill_color(color)
+        .fill();
+    gfx.render_to(&rt, &draw);
+    rt.take_inner()
+}
+
+fn init(gfx: &mut Graphics) -> State {
     let (rng, seed) = get_rng(None);
     log::info!("Seed: {}", seed);
+
+    // The texture radiuses are large because we want large textures that look nice when app is maximized
+    let parent_texture = create_circle_texture(gfx, WORK_SIZE.x * 0.5, Color::NAVY);
+    let spawn_texture = create_circle_texture(gfx, WORK_SIZE.x * 0.5, Color::ORANGE);
+
     State {
         rng: rng,
         last_update: 0.0,
+        parent_texture: parent_texture,
+        spawn_texture: spawn_texture,
         nodes: vec![],
         parent_radius: WORK_SIZE.x * 0.02,
-        spawn_radius: WORK_SIZE.y * 0.01,
+        spawn_radius: WORK_SIZE.x * 0.01,
         spawn_max_distance: WORK_SIZE.x * 0.1,
     }
 }
@@ -50,24 +74,31 @@ fn init() -> State {
 
 fn update(app: &mut App, state: &mut State) {
     let curr_time = app.timer.time_since_init();
-    // if curr_time - state.last_update > 0.05 {
+    // if curr_time - state.last_update > 0.001 {
     if curr_time - state.last_update > 1.0 {
         log::debug!("=o=");
 
         let min_distance = state.parent_radius * 1.5;
+        // let distance = state.spawn_max_distance;
         let distance = state.rng.gen_range(min_distance..state.spawn_max_distance);
+        // Need this offset so that spawn are positioned from the center of
+        // parent node (because of how texture image positioning works)
+        let spawn_offset = state.parent_radius * 0.5;
+
         if let Some(node) = state.get_active_node() {
             if node.last_angle < 360.0 {
                 node.last_angle += 30.0;
                 log::debug!("angle: {}", node.last_angle);
 
-                let spawn_x = node.pos.x + node.last_angle.to_radians().cos() * distance;
-                let spawn_y = node.pos.y + node.last_angle.to_radians().sin() * distance;
+                let spawn_x =
+                    node.pos.x + spawn_offset + node.last_angle.to_radians().cos() * distance;
+                let spawn_y =
+                    node.pos.y + spawn_offset + node.last_angle.to_radians().sin() * distance;
                 state.nodes.push(Node {
                     pos: vec2(spawn_x, spawn_y),
                     last_angle: 0.0,
                     active: false,
-                    is_central: false,
+                    is_parent: false,
                 });
             } else {
                 node.active = false;
@@ -80,7 +111,7 @@ fn update(app: &mut App, state: &mut State) {
                 ),
                 last_angle: 0.0,
                 active: true,
-                is_central: true,
+                is_parent: true,
             });
         }
 
@@ -98,19 +129,18 @@ fn draw(
     let draw = &mut get_draw_setup(gfx, WORK_SIZE, false, Color::WHITE);
 
     for node in state.nodes.iter() {
-        let radius: f32;
-        let fill_color: Color;
-        if node.is_central {
-            radius = state.parent_radius;
-            fill_color = Color::GREEN;
+        let texture: &Texture;
+        let size: f32;
+        if node.is_parent {
+            texture = &state.parent_texture;
+            size = state.parent_radius * 2.0;
         } else {
-            radius = state.spawn_radius;
-            fill_color = Color::ORANGE;
+            texture = &state.spawn_texture;
+            size = state.spawn_radius * 2.0;
         }
-        draw.circle(radius)
+        draw.image(&texture)
             .position(node.pos.x, node.pos.y)
-            .fill_color(fill_color)
-            .fill();
+            .size(size, size);
     }
 
 
