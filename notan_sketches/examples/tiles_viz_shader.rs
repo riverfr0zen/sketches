617d06bcfe0ebfe3c329bd351bdeeed1074630ae
Irfan Baig <riverfr0zen@elitemail.org>
2023-04-12 17:36:38 -0400

    WIP: more progress with tile shader

--- notan_sketches/examples/tiles_viz_shader.rs
+++ notan_sketches/examples/tiles_viz_shader.rs
@@ -9,16 +9,19 @@ use notan_sketches::utils::{
 };
 
 const CLEAR_COLOR: Color = Color::BLUE;
-const BG_COLOR: Color = Color::from_rgb(1.0, 0.0, 0.0);
-const TILE_COLOR1: Color = Color::from_rgb(0.043, 0.525, 0.756);
-// const TILE_COLORS: [Color; 6] = [
-//     colors::AEGEAN,
-//     colors::BANANA,
-//     colors::SAFFRON,
-//     colors::SALMON,
-//     colors::OLIVE,
-//     colors::CARMINE,
-// ];
+// const BG_COLOR: Color = Color::RED;
+// const BG_COLOR: Color = Color::from_rgb(0.02, 1.0, 0.42);
+// const BG_COLOR: Color = Color::WHITE;
+const BG_COLOR: Color = colors::STONE;
+// const TILE_COLOR1: Color = Color::from_rgb(0.043, 0.525, 0.756);
+const TILE_COLORS: [Color; 6] = [
+    colors::AEGEAN,
+    colors::BANANA,
+    colors::GRAYPURP,
+    colors::SALMON,
+    colors::OLIVE,
+    colors::CARMINE,
+];
 // const WORK_SIZE: Vec2 = Vec2::new(800.0, 600.0);
 const WORK_SIZE: Vec2 = ScreenDimensions::RES_1080P;
 
@@ -61,9 +64,14 @@ const FRAG: ShaderSource = notan::fragment_shader! {
     void main() {
         vec2 st = gl_FragCoord.xy / vec2(u_resolution_x, u_resolution_y);
         
-        // float pct = distance(st,vec2(0.5));
         // float pct = 0.5-distance(st,vec2(0.5));
-        float pct = 1.0-distance(st,vec2(0.5));
+        // float pct = 1.0-distance(st,vec2(0.5));
+        // float pct = 0.8-distance(st,vec2(0.5));
+        float dist = distance(st,vec2(0.5));
+        float pct = 0.6-(dist * 1.2);
+        if (pct < 0.0) {
+            pct = 0.0;
+        }
 
         vec3 tile_color = vec3(tile_color_r, tile_color_g, tile_color_b);
         vec3 bg_color = vec3(bg_color_r, bg_color_g, bg_color_b);
@@ -71,77 +79,104 @@ const FRAG: ShaderSource = notan::fragment_shader! {
         // vec3 xcolor = mix(bg_color, tile_color, pct);
         vec3 xcolor = mix(bg_color, tile_color, pct * abs(sin(u_time)));
 
-
-        // Plot a line
         color = vec4(xcolor, 1.0);
     }
 "#
 };
 
 
+struct Tile {
+    srt: ShaderRenderTexture,
+    common_ubo: Buffer,
+    tile_colors_ubo: Buffer,
+}
+
 #[derive(AppState)]
 struct State {
     pub pipeline: Pipeline,
-    pub common_ubo: Buffer,
-    pub tile_colors_ubo: Buffer,
-    pub srt: ShaderRenderTexture,
+    // pub common_ubo: Buffer,
+    pub tiles: Vec<Tile>,
+    // pub common_ubos: Vec<Buffer>,
+    // pub tile_color_ubos: Vec<Buffer>,
+    // pub srts: Vec<ShaderRenderTexture>,
 }
 
 fn init(gfx: &mut Graphics) -> State {
     let pipeline = create_shape_pipeline(gfx, Some(&FRAG)).unwrap();
 
-
-    let common_ubo = gfx
-        .create_uniform_buffer(1, "Common")
-        .with_data(&[0.0, WORK_SIZE.x, WORK_SIZE.y])
-        .build()
-        .unwrap();
-
-    let tile_colors_ubo = gfx
-        .create_uniform_buffer(2, "TileColors")
-        // .with_data(&[TILE_COLOR1, BG_COLOR])
-        .with_data(&[
-            TILE_COLOR1.r,
-            TILE_COLOR1.g,
-            TILE_COLOR1.b,
-            BG_COLOR.r,
-            BG_COLOR.g,
-            BG_COLOR.b,
-        ])
-        .build()
-        .unwrap();
-
-
-    let srt = ShaderRenderTexture::new(gfx, WORK_SIZE.x, WORK_SIZE.y);
-
-    State {
-        pipeline,
-        common_ubo,
-        tile_colors_ubo,
-        srt,
+    let mut tiles: Vec<Tile> = vec![];
+    for tile_color in TILE_COLORS.iter() {
+        tiles.push(Tile {
+            srt: ShaderRenderTexture::new(gfx, WORK_SIZE.x, WORK_SIZE.y),
+            common_ubo: gfx
+                .create_uniform_buffer(1, "Common")
+                .with_data(&[0.0, WORK_SIZE.x, WORK_SIZE.y])
+                .build()
+                .unwrap(),
+            tile_colors_ubo: gfx
+                .create_uniform_buffer(2, "TileColors")
+                .with_data(&[
+                    tile_color.r,
+                    tile_color.g,
+                    tile_color.b,
+                    BG_COLOR.r,
+                    BG_COLOR.g,
+                    BG_COLOR.b,
+                ])
+                .build()
+                .unwrap(),
+        });
     }
+
+    State { pipeline, tiles }
 }
 
 
 fn draw(app: &mut App, gfx: &mut Graphics, state: &mut State) {
     let draw = &mut get_draw_setup(gfx, WORK_SIZE, false, CLEAR_COLOR);
 
-    state.srt.draw_filled(
-        gfx,
-        &state.pipeline,
-        vec![&state.common_ubo, &state.tile_colors_ubo],
-    );
+    for tile in state.tiles.iter_mut() {
+        tile.srt.draw_filled(
+            gfx,
+            &state.pipeline,
+            vec![&tile.common_ubo, &tile.tile_colors_ubo],
+        );
+    }
 
-
-    draw.image(&state.srt.rt)
+    let tile_width = WORK_SIZE.x / 3.0;
+    let tile_height = WORK_SIZE.y / 2.0;
+    draw.image(&state.tiles[0].srt.rt)
         .position(0.0, 0.0)
-        .size(WORK_SIZE.x, WORK_SIZE.y);
+        .size(tile_width, tile_height);
+
+    draw.image(&state.tiles[1].srt.rt)
+        .position(tile_width, 0.0)
+        .size(tile_width, tile_height);
+
+    draw.image(&state.tiles[2].srt.rt)
+        .position(tile_width * 2.0, 0.0)
+        .size(tile_width, tile_height);
+
+    draw.image(&state.tiles[3].srt.rt)
+        .position(0.0, tile_height)
+        .size(tile_width, tile_height);
+
+    draw.image(&state.tiles[4].srt.rt)
+        .position(tile_width, tile_height)
+        .size(tile_width, tile_height);
+
+    draw.image(&state.tiles[5].srt.rt)
+        .position(tile_width * 2.0, tile_height)
+        .size(tile_width, tile_height);
 
 
     gfx.render(draw);
 
     let u_time = app.timer.time_since_init();
-    gfx.set_buffer_data(&state.common_ubo, &[u_time, WORK_SIZE.x, WORK_SIZE.y]);
+
+    for tile in state.tiles.iter() {
+        gfx.set_buffer_data(&tile.common_ubo, &[u_time, WORK_SIZE.x, WORK_SIZE.y]);
+    }
 }
 
 #[notan_main]
