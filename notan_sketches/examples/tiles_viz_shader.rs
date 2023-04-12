e465d2e14f25efff7a3f270514fd62599f8a16a8
Irfan Baig <riverfr0zen@elitemail.org>
2023-04-12 15:44:01 -0400

    WIP: tile shader before adding multiple srts

--- notan_sketches/examples/tiles_viz_shader.rs
+++ notan_sketches/examples/tiles_viz_shader.rs
@@ -1,13 +1,24 @@
 use notan::draw::*;
 use notan::log;
-use notan::math::Vec2;
+use notan::math::{Vec2, Vec3, Vec4};
 use notan::prelude::*;
+use notan_sketches::colors;
 use notan_sketches::shaderutils::ShaderRenderTexture;
 use notan_sketches::utils::{
     get_common_win_config, get_draw_setup, set_html_bgcolor, ScreenDimensions,
 };
 
 const CLEAR_COLOR: Color = Color::BLUE;
+const BG_COLOR: Color = Color::from_rgb(1.0, 0.0, 0.0);
+const TILE_COLOR1: Color = Color::from_rgb(0.043, 0.525, 0.756);
+// const TILE_COLORS: [Color; 6] = [
+//     colors::AEGEAN,
+//     colors::BANANA,
+//     colors::SAFFRON,
+//     colors::SALMON,
+//     colors::OLIVE,
+//     colors::CARMINE,
+// ];
 // const WORK_SIZE: Vec2 = Vec2::new(800.0, 600.0);
 const WORK_SIZE: Vec2 = ScreenDimensions::RES_1080P;
 
@@ -21,11 +32,26 @@ const FRAG: ShaderSource = notan::fragment_shader! {
     layout(location = 0) in vec4 v_color;
     layout(location = 0) out vec4 color;
 
-    layout(set = 0, binding = 1) uniform Common {
+    // layout(set = 0, binding = 0) uniform TileColors {
+    //     vec3 tile_color;
+    //     vec3 bg_color;
+    // };
+
+    layout(set = 0, binding = 0) uniform Common {
         float u_time;
         float u_resolution_x;
         float u_resolution_y;
     };
+
+    layout(set = 0, binding = 0) uniform TileColors {
+        float tile_color_r;
+        float tile_color_g;
+        float tile_color_b;
+        float bg_color_r;
+        float bg_color_g;
+        float bg_color_b;
+    };
+
 
     // Plot a line on Y using a value between 0.0-1.0
     float plot(vec2 st) {    
@@ -36,22 +62,18 @@ const FRAG: ShaderSource = notan::fragment_shader! {
         vec2 st = gl_FragCoord.xy / vec2(u_resolution_x, u_resolution_y);
         
         // float pct = distance(st,vec2(0.5));
+        // float pct = 0.5-distance(st,vec2(0.5));
         float pct = 1.0-distance(st,vec2(0.5));
 
-        vec3 tile_clr = vec3(0.043, 0.525, 0.756);
-        vec3 bg_clr = vec3(1.0, 0.0, 0.0);
+        vec3 tile_color = vec3(tile_color_r, tile_color_g, tile_color_b);
+        vec3 bg_color = vec3(bg_color_r, bg_color_g, bg_color_b);
 
-        // xcolor = vec3(pct);
-        // xcolor = xcolor * pct;
-        // xcolor = xcolor * (pct * abs(sin(u_time)));
-        // vec3 xcolor = vec3(pct * abs(0.5 * sin(u_time)));
-
-        vec3 xcolor = mix(bg_clr, tile_clr, pct);
-        // vec3 xcolor = mix(bg_clr, tile_clr, pct * abs(sin(u_time)));
+        // vec3 xcolor = mix(bg_color, tile_color, pct);
+        vec3 xcolor = mix(bg_color, tile_color, pct * abs(sin(u_time)));
 
 
         // Plot a line
-        color = vec4(xcolor,1.0);
+        color = vec4(xcolor, 1.0);
     }
 "#
 };
@@ -61,6 +83,7 @@ const FRAG: ShaderSource = notan::fragment_shader! {
 struct State {
     pub pipeline: Pipeline,
     pub common_ubo: Buffer,
+    pub tile_colors_ubo: Buffer,
     pub srt: ShaderRenderTexture,
 }
 
@@ -74,11 +97,27 @@ fn init(gfx: &mut Graphics) -> State {
         .build()
         .unwrap();
 
+    let tile_colors_ubo = gfx
+        .create_uniform_buffer(2, "TileColors")
+        // .with_data(&[TILE_COLOR1, BG_COLOR])
+        .with_data(&[
+            TILE_COLOR1.r,
+            TILE_COLOR1.g,
+            TILE_COLOR1.b,
+            BG_COLOR.r,
+            BG_COLOR.g,
+            BG_COLOR.b,
+        ])
+        .build()
+        .unwrap();
+
+
     let srt = ShaderRenderTexture::new(gfx, WORK_SIZE.x, WORK_SIZE.y);
 
     State {
         pipeline,
         common_ubo,
+        tile_colors_ubo,
         srt,
     }
 }
@@ -87,9 +126,11 @@ fn init(gfx: &mut Graphics) -> State {
 fn draw(app: &mut App, gfx: &mut Graphics, state: &mut State) {
     let draw = &mut get_draw_setup(gfx, WORK_SIZE, false, CLEAR_COLOR);
 
-    state
-        .srt
-        .draw_filled(gfx, &state.pipeline, vec![&state.common_ubo]);
+    state.srt.draw_filled(
+        gfx,
+        &state.pipeline,
+        vec![&state.common_ubo, &state.tile_colors_ubo],
+    );
 
 
     draw.image(&state.srt.rt)
