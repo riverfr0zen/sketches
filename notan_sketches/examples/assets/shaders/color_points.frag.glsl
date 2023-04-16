f877d7d82830a4cbdcc729621ca405180740654f
Irfan Baig <riverfr0zen@elitemail.org>
2023-04-16 17:15:39 -0400

    simplify types in shader

--- notan_sketches/examples/assets/shaders/color_points.frag.glsl
+++ notan_sketches/examples/assets/shaders/color_points.frag.glsl
@@ -11,21 +11,15 @@ layout(set = 0, binding = 0) uniform Common {
 };
 
 layout(set = 0, binding = 0) uniform BgColor {
-    float bg_color_r;
-    float bg_color_g;
-    float bg_color_b;
+    vec3 bg_color;
 };
 
 layout(set = 0, binding = 0) uniform ColorSource1 {
-    float color1_r;
-    float color1_g;
-    float color1_b;
+    vec3 color1;
 };
 
 layout(set = 0, binding = 0) uniform ColorSource2 {
-    float color2_r;
-    float color2_g;
-    float color2_b;
+    vec3 color2;
 };
 
 
@@ -36,8 +30,8 @@ float get_distance(in vec2 point1, in vec2 point2) {
 
 
 float get_pct(in float distance) {
-    // float pct = 0.6-(distance * 1.5);
-    float pct = abs(sin(u_time))-(distance * (1.0 + abs(sin(u_time))));
+    float pct = 0.6-(distance * 1.5);
+    // float pct = abs(sin(u_time))-(distance * (1.0 + abs(sin(u_time))));
     if (pct < 0.0) {
         return 0.0;
     }
@@ -49,14 +43,9 @@ void main() {
     
     float color1_dist = get_distance(st,vec2(0.2, 0.8));
     float color1_pct = get_pct(color1_dist);
-    vec3 color1 = vec3(color1_r, color1_g, color1_b);
-
 
     float color2_dist = get_distance(st,vec2(0.5, 0.5));
     float color2_pct = get_pct(color2_dist);
-    vec3 color2 = vec3(color2_r, color2_g, color2_b);
-
-    vec3 bg_color = vec3(bg_color_r, bg_color_g, bg_color_b);
 
     vec3 xcolor = mix(bg_color, color1, color1_pct);
     // vec3 xcolor = mix(bg_color, color1, color1_pct * abs(sin(u_time)));
