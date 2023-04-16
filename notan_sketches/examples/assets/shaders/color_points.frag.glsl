b6e06119fc0242c6a7feddaf0abefb547cda0293
Irfan Baig <riverfr0zen@elitemail.org>
2023-04-16 17:11:47 -0400

    extracting some functions in shader

--- notan_sketches/examples/assets/shaders/color_points.frag.glsl
+++ notan_sketches/examples/assets/shaders/color_points.frag.glsl
@@ -29,26 +29,31 @@ layout(set = 0, binding = 0) uniform ColorSource2 {
 };
 
 
+float get_distance(in vec2 point1, in vec2 point2) {
+    return distance(point1, point2);
+    // return distance(point1, vec2(point2.x, abs(sin(u_time))));
+}
+
+
+float get_pct(in float distance) {
+    // float pct = 0.6-(distance * 1.5);
+    float pct = abs(sin(u_time))-(distance * (1.0 + abs(sin(u_time))));
+    if (pct < 0.0) {
+        return 0.0;
+    }
+    return pct;
+}
+
 void main() {
     vec2 st = gl_FragCoord.xy / vec2(u_resolution_x, u_resolution_y);
     
-    float color1_dist = distance(st,vec2(0.2, 0.8));
-    // float dist = distance(st,vec2(0.2, abs(sin(u_time))));
-    float color1_pct = 0.6-(color1_dist * 1.5);
-    // float color1_pct = abs(sin(u_time))-(color1_dist * (1.0 + abs(sin(u_time))));
-    if (color1_pct < 0.0) {
-        color1_pct = 0.0;
-    }
+    float color1_dist = get_distance(st,vec2(0.2, 0.8));
+    float color1_pct = get_pct(color1_dist);
     vec3 color1 = vec3(color1_r, color1_g, color1_b);
 
 
-    float color2_dist = distance(st,vec2(0.5, 0.5));
-    // float dist = distance(st,vec2(0.2, abs(sin(u_time))));
-    float color2_pct = 0.6-(color2_dist * 1.5);
-    // float color2_pct = abs(sin(u_time))-(color2_dist * (1.0 + abs(sin(u_time))));
-    if (color2_pct < 0.0) {
-        color2_pct = 0.0;
-    }
+    float color2_dist = get_distance(st,vec2(0.5, 0.5));
+    float color2_pct = get_pct(color2_dist);
     vec3 color2 = vec3(color2_r, color2_g, color2_b);
 
     vec3 bg_color = vec3(bg_color_r, bg_color_g, bg_color_b);
